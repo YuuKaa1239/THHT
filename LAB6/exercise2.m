52f2f33728caf204283001980da14f1ef66508fa
Trần Thanh Khoa <87396977+YuuKaa1239@users.noreply.github.com>
2023-11-28 21:56:50 +0700

    Update exercise2.m

--- LAB6/exercise2.m
+++ LAB6/exercise2.m
@@ -5,10 +5,10 @@ H=freqz(b,a,w1);
 figure(1)
 plot(w1/pi,20*log10(abs(H)))
 ylabel("Chuẩn log (dB)")
-xlabel("Phân bố chuẩn (\times\pi rad/sample)")
+xlabel("radian/persample (\times\pi rad/sample)")
 w2=[0:2*pi/4:2*pi];
 H1=freqz(b,a,w2);
 figure(2)
 plot(w2/pi,20*log10(abs(H1)))
 ylabel("Chuẩn log (dB)")
-xlabel("Phân bố chuẩn (\times\pi rad/sample)")
+xlabel("radian/persample (\times\pi rad/sample)")
